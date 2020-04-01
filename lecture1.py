7efd756c8a179e6c0884db346e6bc0127f22258e
jjunsheng <jjunsheng95@gmail.com>
2020-04-01 21:54:12 +0800

    lecture1

---
+++ lecture1.py
@@ -0,0 +1,80 @@
+# hello.py # By Joseph Tonien
+# My first Python program
+
+print("Hello World")
+a = 1
+b = 2
+c = a + b
+print(c)
+
+# variables are reserved memory locations to store value
+
+#diamond.py
+#By Joseph Tonien
+
+#A program to demonstrate the use of print function
+# Print out a shape of a diamond
+
+print("     *")
+print("    * *")
+print("   *   *")
+print("  *     *")
+print(" *       *")
+print("*         *")
+print(" *       *")
+print("  *     *")
+print("   *   *")
+print("    * *")
+print("     *")
+
+
+# A program to demonstrate the use of print function # Print out Unimovies program
+print("Welcome to Unimovies!");
+print()
+print("Thursday July 30 at 7.15pm: Inside Out")
+print()
+print("Director: Pete Docter, Ronaldo Del Carmen")
+print("Writer:   Pete Docter, Meg LeFauve")
+print("Starring: Diane Lane, Amy Poehler, Mindy Kaling")
+print("Released: June 18, 2015")
+print("Rating:   PG")
+print("Runtime:  102 minutes")
+print("Websites: http://movieweb.com/movie/inside-out-2015")
+
+
+# fullname.py
+first_name = "John"
+last_name = "Smith"
+full_name = first_name + " " + last_name
+print("My name is " + full_name + ".")
+
+
+# subject.py
+subject1 = "ISIT111"
+subject2 = "MATH101"
+subject3 = "ACCY113"
+print("My enrolled subjects are: " + subject1 + ", " + subject2 + ", " + subject3 + ".")
+
+# favorite_number.py
+fav_number = 7
+# print("My favorite number is " + fav_number)
+print("My favorite number is " + str(fav_number))
+
+# sum_of_two_numbers.py
+number1 = 10
+number2 = 20
+sum = number1 + number2
+print("The sum of " + str(number1) + " and " + str(number2) + " is " + str(sum) + ".")
+
+
+
+# SECOND_PER_MINUTE = 60
+# minute = 5
+# second = minute * SECOND_PER_MINUTE
+# print(str(minute) + " minutes has " + str(second) + " seconds")
+#
+# first_name = "John" last_name = "Smith" full_name = first_name + " " + last_name
+# fav_number = 7
+# subject1 = "ISIT111" subject2 = "MATH101" subject3 = "ACCY113"
+# SECOND_PER_MINUTE = 60
+# minute = 5 second = minute * SECOND_PER_MINUT
